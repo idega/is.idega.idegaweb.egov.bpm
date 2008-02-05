75afbaab7cd2e4aedb5819dd269a562ec23b9f49
Vytautas Civilis <civilis@idega.com>
2008-02-05 09:13:51 +0000

    *** empty log message ***

---
+++ src/java/is/idega/idegaweb/egov/bpm/application/ApplicationTypeBPM.java
@@ -0,0 +1,65 @@
+package is.idega.idegaweb.egov.bpm.application;
+
+import is.idega.idegaweb.egov.application.business.ApplicationType;
+import is.idega.idegaweb.egov.application.business.ApplicationTypePluggedInEvent;
+import is.idega.idegaweb.egov.application.data.Application;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import com.idega.presentation.IWContext;
+
+/**
+ * Interface is meant to be extended by beans, reflecting application type for egov applications
+ * 
+ * @author <a href="civilis@idega.com">Vytautas Čivilis</a>
+ * @version $Revision: 1.1 $
+ *
+ * Last modified: $Date: 2008/02/05 09:13:51 $ by $Author: civilis $
+ *
+ */
+public class ApplicationTypeBPM implements ApplicationType, ApplicationContextAware, ApplicationListener {
+
+	private ApplicationContext ctx;
+	private static final String beanIdentifier = "appTypeBPM";
+	private static final String appType = "EGOV_BPM";
+	
+	public UIComponent getHandlerComponent(FacesContext ctx) {
+		return null;
+	}
+
+	public String getLabel(IWContext iwc) {
+		return "EGOV BPM";
+	}
+
+	public String getType() {
+		return appType;
+	}
+
+	public void save(IWContext iwc, Application app) {
+		
+		System.out.println("saving ..BPM...");
+	}
+
+	public void setApplicationContext(ApplicationContext applicationcontext)
+			throws BeansException {
+		ctx = applicationcontext;		
+	}
+
+	public void onApplicationEvent(ApplicationEvent applicationevent) {
+		
+		if(applicationevent instanceof ContextRefreshedEvent) {
+			
+			ApplicationTypePluggedInEvent event = new ApplicationTypePluggedInEvent(this);
+			event.setAppTypeBeanIdentifier(beanIdentifier);
+			ctx.publishEvent(event);
+		}
+	}
+}
