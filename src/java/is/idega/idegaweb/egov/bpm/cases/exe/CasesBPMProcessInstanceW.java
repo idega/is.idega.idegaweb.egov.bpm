3e5c99bfe1576168a82588d768f702c02a676315
Arunas <arunas@idega.com>
2008-09-11 13:33:37 +0000

    getStartedTaskName added

--- src/java/is/idega/idegaweb/egov/bpm/cases/exe/CasesBPMProcessInstanceW.java
+++ src/java/is/idega/idegaweb/egov/bpm/cases/exe/CasesBPMProcessInstanceW.java
@@ -12,6 +12,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,6 +31,7 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import com.idega.block.form.process.XFormsView;
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
 import com.idega.business.IBORuntimeException;
@@ -48,6 +50,7 @@ import com.idega.jbpm.identity.BPMUser;
 import com.idega.jbpm.identity.Role;
 import com.idega.jbpm.identity.permission.BPMTypedPermission;
 import com.idega.jbpm.identity.permission.PermissionsFactory;
+import com.idega.jbpm.view.View;
 import com.idega.presentation.IWContext;
 import com.idega.user.business.UserBusiness;
 import com.idega.user.data.User;
@@ -58,9 +61,9 @@ import com.idega.util.CoreUtil;
  * TODO: we could create abstract class for some generic methods, like getPeopleConntectedToTheProcess
  * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.12 $
+ * @version $Revision: 1.13 $
  *
- * Last modified: $Date: 2008/09/09 06:02:43 $ by $Author: arunas $
+ * Last modified: $Date: 2008/09/11 13:33:37 $ by $Author: arunas $
  */
 @Scope("prototype")
 @Service("casesPIW")
@@ -264,7 +267,7 @@ public class CasesBPMProcessInstanceW implements ProcessInstanceW {
 	public String getCaseIdentifier() {
 	    
 	    if (processInstanceId == null) {
-		return null;
+	    	return null;
 	    }
 	    
 	    JbpmContext ctx = getIdegaJbpmContext().createJbpmContext();
@@ -278,7 +281,23 @@ public class CasesBPMProcessInstanceW implements ProcessInstanceW {
 		getIdegaJbpmContext().closeAndCommit(ctx);
 	    }
 	    
-	    
+	}
+	
+	public String getStartedTaskName() {
+		
+		 if (processInstanceId == null) {
+		    	return null;
+		 }
+		 
+		List<String> preferred = new ArrayList<String>(1);
+		preferred.add(XFormsView.VIEW_TYPE);
+		
+		Long taskId = getProcessInstance().getProcessDefinition().getTaskMgmtDefinition().getStartTask().getId();
+		
+		View view = getBpmFactory().getViewByTask(taskId, false, preferred);
+		
+		return view.getDisplayName(new Locale("is","IS"));
+		
 	}
 	
 	public Integer getHandlerId() {
