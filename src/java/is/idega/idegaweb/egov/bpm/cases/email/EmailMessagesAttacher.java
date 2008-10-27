e887f42764158689819d45bccc662111a9314f51
Juozas Zabuka <juozas@idega.com>
2008-10-27 14:21:11 +0000

    Fixed attachments

--- src/java/is/idega/idegaweb/egov/bpm/cases/email/EmailMessagesAttacher.java
+++ src/java/is/idega/idegaweb/egov/bpm/cases/email/EmailMessagesAttacher.java
@@ -56,9 +56,9 @@ import com.idega.util.StringUtil;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.12 $
+ * @version $Revision: 1.13 $
  *
- * Last modified: $Date: 2008/10/27 12:46:14 $ by $Author: juozas $
+ * Last modified: $Date: 2008/10/27 14:21:11 $ by $Author: juozas $
  */
 @Scope("singleton")
 @Service
@@ -274,6 +274,7 @@ public class EmailMessagesAttacher implements ApplicationListener {
 							fileName =  MimeUtility.decodeText(fileName);
 							
 						}else{
+							//well not much can be done then can it?:)
 							fileName = "UnknownFile";
 						}
 						attachemntMap.put(fileName, input);
@@ -339,138 +340,10 @@ public class EmailMessagesAttacher implements ApplicationListener {
 	}
 	
 	private String parsePlainTextMessage(String message){
-		return escapeHTMLSpecialChars(message);
+		return StringUtil.escapeHTMLSpecialChars(message);
 	}
 	
-	//TODO: move it somewhere
-	public String escapeHTMLSpecialChars(String aText){
-	     StringBuilder result = new StringBuilder();
-	     StringCharacterIterator iterator = new StringCharacterIterator(aText);
-	     char character =  iterator.current();
-	     while (character != CharacterIterator.DONE ){
-	       if (character == '<') {
-	         result.append("&lt;");
-	       }
-	       else if (character == '>') {
-	         result.append("&gt;");
-	       }
-	       else if (character == '&') {
-	         result.append("&amp;");
-	      }
-	       else if (character == '\"') {
-	         result.append("&quot;");
-	       }
-	       else if (character == '\t') {
-	         addCharEntity(9, result);
-	       }
-	       else if (character == '!') {
-	         addCharEntity(33, result);
-	       }
-	       else if (character == '#') {
-	         addCharEntity(35, result);
-	       }
-	       else if (character == '$') {
-	         addCharEntity(36, result);
-	       }
-	       else if (character == '%') {
-	         addCharEntity(37, result);
-	       }
-	       else if (character == '\'') {
-	         addCharEntity(39, result);
-	       }
-	       else if (character == '(') {
-	         addCharEntity(40, result);
-	       }
-	       else if (character == ')') {
-	         addCharEntity(41, result);
-	       }
-	       else if (character == '*') {
-	         addCharEntity(42, result);
-	       }
-	       else if (character == '+') {
-	         addCharEntity(43, result);
-	       }
-	       else if (character == ',') {
-	         addCharEntity(44, result);
-	       }
-	       else if (character == '-') {
-	         addCharEntity(45, result);
-	       }
-	       else if (character == '.') {
-	         addCharEntity(46, result);
-	       }
-	       else if (character == '/') {
-	         addCharEntity(47, result);
-	       }
-	       else if (character == ':') {
-	         addCharEntity(58, result);
-	       }
-	       else if (character == ';') {
-	         addCharEntity(59, result);
-	       }
-	       else if (character == '=') {
-	         addCharEntity(61, result);
-	       }
-	       else if (character == '?') {
-	         addCharEntity(63, result);
-	       }
-	       else if (character == '@') {
-	         addCharEntity(64, result);
-	       }
-	       else if (character == '[') {
-	         addCharEntity(91, result);
-	       }
-	       else if (character == '\\') {
-	         addCharEntity(92, result);
-	       }
-	       else if (character == ']') {
-	         addCharEntity(93, result);
-	       }
-	       else if (character == '^') {
-	         addCharEntity(94, result);
-	       }
-	       else if (character == '_') {
-	         addCharEntity(95, result);
-	       }
-	       else if (character == '`') {
-	         addCharEntity(96, result);
-	       }
-	       else if (character == '{') {
-	         addCharEntity(123, result);
-	       }
-	       else if (character == '|') {
-	         addCharEntity(124, result);
-	       }
-	       else if (character == '}') {
-	         addCharEntity(125, result);
-	       }
-	       else if (character == '~') {
-	         addCharEntity(126, result);
-	       }
-	       else {
-	         //the char is not a special one
-	         //add it to the result as is
-	         result.append(character);
-	       }
-	       character = iterator.next();
-	     }
-	     return result.toString();
-	  }
 	
-	private void addCharEntity(Integer aIdx, StringBuilder aBuilder){
-	    String padding = "";
-	    if( aIdx <= 9 ){
-	       padding = "00";
-	    }
-	    else if( aIdx <= 99 ){
-	      padding = "0";
-	    }
-	    else {
-	      //no prefix
-	    }
-	    String number = padding + aIdx.toString();
-	    aBuilder.append("&#" + number + ";");
-	}
 	
 	class PISFORMSG {
 
