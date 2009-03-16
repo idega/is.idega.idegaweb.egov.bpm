52670ec7a0627ae1b12a5d1ea31bf9ced817636d
Juozas Zabuka <juozas@idega.com>
2009-03-16 11:27:52 +0000

    added getProcessAttachments

--- src/java/is/idega/idegaweb/egov/bpm/xform/bean/XformBPMDSBean.java
+++ src/java/is/idega/idegaweb/egov/bpm/xform/bean/XformBPMDSBean.java
@@ -11,26 +11,26 @@ import org.springframework.stereotype.Service;
 import com.idega.core.contact.data.Email;
 import com.idega.jbpm.exe.BPMFactory;
 import com.idega.jbpm.exe.ProcessInstanceW;
+import com.idega.jbpm.variables.BinaryVariable;
 import com.idega.user.data.User;
 import com.idega.util.text.Item;
 
 /**
  * @author <a href="mailto:arunas@idega.com">Arūnas Vasmanas</a>
- * @version $Revision: 1.5 $
- *
- * Last modified: $Date: 2009/02/09 11:54:53 $ by $Author: arunas $
+ * @version $Revision: 1.6 $ Last modified: $Date: 2009/03/16 11:27:52 $ by $Author: juozas $
  */
 
 @Scope("singleton")
 @Service("xformBPM")
-public class XformBPMDSBean implements XformBPM{
-
-	@Autowired private BPMFactory bpmFactory;
-		
-	protected BPMFactory getBpmFactory() {		
+public class XformBPMDSBean implements XformBPM {
+	
+	@Autowired
+	private BPMFactory bpmFactory;
+	
+	protected BPMFactory getBpmFactory() {
 		return bpmFactory;
 	}
-
+	
 	protected void setBpmFactory(BPMFactory bpmFactory) {
 		this.bpmFactory = bpmFactory;
 	}
@@ -41,9 +41,9 @@ public class XformBPMDSBean implements XformBPM{
 		
 		List<Item> usersItem = new ArrayList<Item>();
 		
-		for(User user: users) 
+		for (User user : users)
 			usersItem.add(new Item(user.getId(), user.getName()));
-			
+		
 		return usersItem;
 	}
 	
@@ -53,9 +53,9 @@ public class XformBPMDSBean implements XformBPM{
 		
 		List<Item> usersItem = new ArrayList<Item>();
 		
-		for(User user: users) 
+		for (User user : users)
 			usersItem.add(new Item(user.getName(), user.getName()));
-			
+		
 		return usersItem;
 	}
 	
@@ -66,24 +66,26 @@ public class XformBPMDSBean implements XformBPM{
 		
 		List<Item> usersItem = new ArrayList<Item>();
 		
-		for(User user: users) 
-			usersItem.add(new Item(getUserEmails(user.getEmails()),user.getName()));
-			
+		for (User user : users)
+			usersItem.add(new Item(getUserEmails(user.getEmails()), user
+			        .getName()));
+		
 		return usersItem;
 	}
 	
 	private List<User> getUsersConnectedList(Long pid) {
 		
 		ProcessInstanceW piw = getBpmFactory()
-		.getProcessManagerByProcessInstanceId(pid)
-		.getProcessInstance(pid);
+		        .getProcessManagerByProcessInstanceId(pid).getProcessInstance(
+		            pid);
 		
-		Collection<User> peopleConnectedToProcess = piw.getUsersConnectedToProcess();
+		Collection<User> peopleConnectedToProcess = piw
+		        .getUsersConnectedToProcess();
 		
 		List<User> uniqueUsers = new ArrayList<User>();
 		
 		if (peopleConnectedToProcess != null) {
-			for(User user: peopleConnectedToProcess) {
+			for (User user : peopleConnectedToProcess) {
 				if (!uniqueUsers.contains(user)) {
 					uniqueUsers.add(user);
 				}
@@ -96,11 +98,28 @@ public class XformBPMDSBean implements XformBPM{
 		
 		StringBuilder userEmails = new StringBuilder();
 		
-		for (Email email : emails) 
+		for (Email email : emails)
 			userEmails.append(email.getEmailAddress());
 		
 		return userEmails.toString();
 		
 	}
-
+	
+	public List<Item> getProcessAttachments(String pid) {
+		
+		int processInstanceId = Integer.valueOf(pid);
+		
+		ProcessInstanceW piw = getBpmFactory()
+		        .getProcessManagerByProcessInstanceId(processInstanceId)
+		        .getProcessInstance(processInstanceId);
+		
+		List<Item> attachments = new ArrayList<Item>();
+		
+		for (BinaryVariable binaryVariable : piw.getAttachements())
+			attachments.add(new Item(binaryVariable.getTaskInstanceId() + ";"
+			        + binaryVariable.getHash(), binaryVariable.getFileName()));
+		
+		return attachments;
+	}
+	
 }
