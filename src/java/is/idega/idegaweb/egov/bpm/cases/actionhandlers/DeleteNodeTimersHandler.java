4dcc7946d69d40c94b4922ebc1d805c11ca2ee87
Vytautas Civilis <civilis@idega.com>
2009-03-20 19:21:53 +0000

    support for deleting timers in different nodes than token current

--- src/java/is/idega/idegaweb/egov/bpm/cases/actionhandlers/DeleteNodeTimersHandler.java
+++ src/java/is/idega/idegaweb/egov/bpm/cases/actionhandlers/DeleteNodeTimersHandler.java
@@ -3,6 +3,7 @@ package is.idega.idegaweb.egov.bpm.cases.actionhandlers;
 import java.util.List;
 
 import org.jbpm.graph.def.ActionHandler;
+import org.jbpm.graph.def.Node;
 import org.jbpm.graph.exe.ExecutionContext;
 import org.jbpm.graph.exe.Token;
 import org.jbpm.scheduler.SchedulerService;
@@ -10,32 +11,52 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 /**
- * 
- * 
  * @author <a href="anton@idega.com">Anton Makarov</a>
- * @version Revision: 1.0
- * 
- * Last modified: Nov 28, 2008 by Author: Anton
- * 
+ * @version Revision: 1.0 Last modified: Nov 28, 2008 by Author: Anton
  */
 
 @Service("deleteNodeTimersHandler")
 @Scope("prototype")
 public class DeleteNodeTimersHandler implements ActionHandler {
-
+	
 	private static final long serialVersionUID = -8838368363278394618L;
-
+	
+	private String timersNodeName;
+	
 	@SuppressWarnings("unchecked")
 	public void execute(ExecutionContext executionContext) throws Exception {
+		
 		SchedulerService schedulerService = executionContext.getJbpmContext()
-				.getServices().getSchedulerService();
-
+		        .getServices().getSchedulerService();
+		
+		Node timersNode = null;
+		
+		if (getTimersNodeName() != null) {
+			
+			timersNode = executionContext.getProcessDefinition().getNode(
+			    getTimersNodeName());
+		} else {
+			timersNode = executionContext.getNode();
+		}
+		
+		String timersName = timersNode.getName();
 		List<Token> tokens = executionContext.getProcessInstance()
-				.getRootToken().getChildrenAtNode(executionContext.getNode());
+		        .getRootToken().getChildrenAtNode(timersNode);
+		
 		for (Token tok : tokens) {
+			
 			if (!tok.equals(executionContext.getToken())) {
-				schedulerService.deleteTimersByName(executionContext.getNode().getName(), tok);	
+				
+				schedulerService.deleteTimersByName(timersName, tok);
 			}
 		}
+	}
+	
+	public String getTimersNodeName() {
+		return timersNodeName;
+	}
+	
+	public void setTimersNodeName(String timersNodeName) {
+		this.timersNodeName = timersNodeName;
 	}
 }
