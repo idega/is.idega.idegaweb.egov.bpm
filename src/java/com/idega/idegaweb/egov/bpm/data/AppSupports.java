822b2964dd04758530a146104923297b323b4c08
Vytautas Civilis <civilis@idega.com>
2009-04-29 13:39:00 +0000

    old query replaced with getSetByApplicationId

--- src/java/com/idega/idegaweb/egov/bpm/data/AppSupports.java
+++ src/java/com/idega/idegaweb/egov/bpm/data/AppSupports.java
@@ -15,74 +15,68 @@ import javax.persistence.Table;
  * Binds process with roles, that can start process.
  * 
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.3 $
- * 
- *          Last modified: $Date: 2008/12/11 19:24:45 $ by $Author: civilis $
+ * @version $Revision: 1.4 $ Last modified: $Date: 2009/04/29 13:39:00 $ by $Author: civilis $
  */
 @Entity
 @Table(name = "BPM_APPLICATIONS_SUPPORTS")
-@NamedQueries( { @NamedQuery(name = AppSupports.getSetByProcessNameApplicationId, query = "from AppSupports asup where asup."
-		+ AppSupports.processNameProperty
-		+ " = :"
-		+ AppSupports.processNameProperty
-		+ " and asup."
-		+ AppSupports.applicationIdProperty
-		+ " = :"
-		+ AppSupports.applicationIdProperty) })
+@NamedQueries( { @NamedQuery(name = AppSupports.getSetByApplicationId, query = "from AppSupports asup where asup."
+        + AppSupports.applicationIdProperty
+        + " = :"
+        + AppSupports.applicationIdProperty) })
 public class AppSupports implements Serializable {
-
+	
 	private static final long serialVersionUID = -1937482511922158268L;
-
-	public static final String getSetByProcessNameApplicationId = "AppSupports.getSetByProcessNameApplicationId";
-
+	
+	public static final String getSetByApplicationId = "AppSupports.getSetByApplicationId";
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "ID_")
 	private Long id;
-
+	
 	public static final String processNameProperty = "processName";
 	@Column(name = "PROCESS_NAME", nullable = false)
 	private String processName;
-
+	
 	public static final String applicationIdProperty = "applicationId";
 	@Column(name = "APPLICATION_ID", nullable = false)
 	private Integer applicationId;
-
+	
 	public static final String roleKeyProperty = "roleKey";
 	@Column(name = "ROLE_KEY", nullable = false)
 	private String roleKey;
-
+	
 	public Long getId() {
 		return id;
 	}
-
+	
 	public Integer getApplicationId() {
 		return applicationId;
 	}
-
+	
 	public void setApplicationId(Integer applicationId) {
 		this.applicationId = applicationId;
 	}
-
+	
 	public String getRoleKey() {
 		return roleKey;
 	}
-
+	
 	public void setRoleKey(String roleKey) {
 		this.roleKey = roleKey;
 	}
-
+	
 	@Override
 	public String toString() {
-
+		
 		return "application id=" + getApplicationId() + " role key="
-				+ getRoleKey();
+		        + getRoleKey();
 	}
-
+	
 	public String getProcessName() {
 		return processName;
 	}
-
+	
 	public void setProcessName(String processName) {
 		this.processName = processName;
 	}
