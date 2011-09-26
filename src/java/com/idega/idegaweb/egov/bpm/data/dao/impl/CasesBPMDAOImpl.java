c5dd5c67735ba984b88be2def61c4f754d7cdfc7
Valdas Žemaitis <valdas@idega.com>
2011-09-26 14:59:20 +0300

    Restoring the queries where case code(s) are treated as BPM process definition name - such logic is already used on live servers...

--- src/java/com/idega/idegaweb/egov/bpm/data/dao/impl/CasesBPMDAOImpl.java
+++ src/java/com/idega/idegaweb/egov/bpm/data/dao/impl/CasesBPMDAOImpl.java
@@ -501,13 +501,13 @@ public class CasesBPMDAOImpl extends GenericDaoImpl implements CasesBPMDAO {
 		builder.append("ni.identity_id = :identityId  and  ni.identity_type = :identityType) ");
 		builder.append("and act.process_instance_id is not null and pi.end_ is null ");
 		builder.append(getConditionForCaseStatuses(params, caseStatusesToShow, caseStatusesToHide));
+		if (!ListUtil.isEmpty(caseCodes))
+			builder.append(" and pi.processdefinition_ in (select id_ from jbpm_processdefinition where name_ in (:caseCodes)) ");
 		if (onlySubscribedCases) {
-			builder.append("and (proc_case.user_id = :caseAuthor or proc_case_subscribers.ic_user_id = :subscriber) ");
+			builder.append(" and (proc_case.user_id = :caseAuthor or proc_case_subscribers.ic_user_id = :subscriber) ");
 			params.add(new Param("subscriber", user.getPrimaryKey()));
 			params.add(new Param("caseAuthor", user.getPrimaryKey()));
 		}
-		if (!ListUtil.isEmpty(caseCodes))
-			builder.append(" and proc_case.case_code in (:caseCodes)");
 		
 		//	The second part of a query
 		builder.append(") union (select distinct comm_case.comm_case_id as caseId, proc_case.created as Created from comm_case inner join proc_case on ")
@@ -540,7 +540,7 @@ public class CasesBPMDAOImpl extends GenericDaoImpl implements CasesBPMDAO {
 		builder.append(" act.process_instance_id is not null and pi.end_ is null ");
 		builder.append(getConditionForCaseStatuses(params, caseStatusesToShow, caseStatusesToHide));
 		if (!ListUtil.isEmpty(caseCodes))
-			builder.append(" and proc_case.case_code in (:caseCodes)");
+			builder.append(" and pi.processdefinition_ in (select id_ from jbpm_processdefinition where name_ in (:caseCodes))");
 		
 		builder.append(") union"
 		                + "(select distinct comm_case.comm_case_id as caseId, proc_case.created as Created from comm_case "
