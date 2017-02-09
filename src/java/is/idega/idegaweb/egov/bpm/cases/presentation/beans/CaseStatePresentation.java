88324c7136399b85a4034b7a7cbf6d9c4507a1da
Arūnas Proscevičius <arunas.proscevicius@gmail.com>
2017-02-09 16:27:11 +0200

    not loading case data fix

--- src/java/is/idega/idegaweb/egov/bpm/cases/presentation/beans/CaseStatePresentation.java
+++ src/java/is/idega/idegaweb/egov/bpm/cases/presentation/beans/CaseStatePresentation.java
@@ -1,10 +1,13 @@
 package is.idega.idegaweb.egov.bpm.cases.presentation.beans;
 
+import java.io.Serializable;
+
 import com.idega.idegaweb.egov.bpm.data.CaseState;
 import com.idega.idegaweb.egov.bpm.data.CaseStateInstance;
 
-public class CaseStatePresentation {
+public class CaseStatePresentation implements Serializable{
 
+	private static final long serialVersionUID = -1201284587530069691L;
 	private CaseStateInstance stateInstance;
 	private CaseState stateDefinition;
 
