02df548cc093b0c2bac5260517ad597e9d268f15
Valdas Žemaitis <valdas@idega.com>
2017-10-27 12:00:35 +0300

    2 new methods: to re-submit variables and re-submit case with variables

--- src/java/com/idega/idegaweb/egov/bpm/business/BPMManipulator.java
+++ src/java/com/idega/idegaweb/egov/bpm/business/BPMManipulator.java
@@ -5,4 +5,8 @@ public interface BPMManipulator {
 	public boolean doReSubmitProcess(Long piId, boolean onlyStart, boolean submitRepeatedTasks);
 	public boolean doReSubmitCase(Integer caseId, boolean onlyStart, boolean submitRepeatedTasks);
 
+	public boolean doReSubmitCaseWithVariables(Integer caseId, boolean onlyStart, boolean submitRepeatedTasks, String variablesEncodedBase64);
+
+	public boolean doSubmitVariables(Integer caseId, String variablesEncodedBase64);
+
 }
--- src/java/com/idega/idegaweb/egov/bpm/business/impl/BPMManipulatorImpl.java
+++ src/java/com/idega/idegaweb/egov/bpm/business/impl/BPMManipulatorImpl.java
@@ -1,9 +1,11 @@
 package com.idega.idegaweb.egov.bpm.business.impl;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -19,10 +21,14 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Base64Utils;
 
 import com.idega.block.process.business.CaseBusiness;
 import com.idega.block.process.data.Case;
 import com.idega.block.process.data.CaseBMPBean;
+import com.idega.block.process.variables.Variable;
+import com.idega.block.process.variables.VariableDataType;
+import com.idega.bpm.xformsview.converters.DataConvertersFactory;
 import com.idega.core.accesscontrol.business.LoginBusinessBean;
 import com.idega.core.accesscontrol.business.LoginDBHandler;
 import com.idega.core.accesscontrol.dao.UserLoginDAO;
@@ -33,6 +39,7 @@ import com.idega.idegaweb.egov.bpm.data.CaseProcInstBind;
 import com.idega.idegaweb.egov.bpm.data.dao.CasesBPMDAO;
 import com.idega.jbpm.BPMContext;
 import com.idega.jbpm.JbpmCallback;
+import com.idega.jbpm.bean.VariableInstanceType;
 import com.idega.jbpm.exe.BPMFactory;
 import com.idega.jbpm.exe.ProcessConstants;
 import com.idega.jbpm.exe.ProcessDefinitionW;
@@ -44,10 +51,12 @@ import com.idega.jbpm.view.ViewSubmission;
 import com.idega.presentation.IWContext;
 import com.idega.user.dao.UserDAO;
 import com.idega.user.data.bean.User;
+import com.idega.util.CoreConstants;
 import com.idega.util.CoreUtil;
 import com.idega.util.ListUtil;
 import com.idega.util.StringHandler;
 import com.idega.util.StringUtil;
+import com.idega.util.datastructures.map.MapUtil;
 import com.idega.util.expression.ELUtil;
 
 @Service(BPMManipulatorImpl.BEAN_NAME)
@@ -70,6 +79,16 @@ public class BPMManipulatorImpl extends DefaultSpringBean implements
 	@Autowired
 	private BPMContext bpmContext;
 
+	@Autowired
+	private DataConvertersFactory convertersFactory;
+
+	private DataConvertersFactory getConvertersFactory() {
+		if (convertersFactory == null) {
+			ELUtil.getInstance().autowire(this);
+		}
+		return convertersFactory;
+	}
+
 	@Override
 	@RemoteMethod
 	public boolean doReSubmitProcess(Long piId, boolean onlyStart,
@@ -86,15 +105,49 @@ public class BPMManipulatorImpl extends DefaultSpringBean implements
 
 	@Override
 	@RemoteMethod
-	public boolean doReSubmitCase(Integer caseId, boolean onlyStart,
-			boolean submitRepeatedTasks) {
+	public boolean doReSubmitCase(Integer caseId, boolean onlyStart, boolean submitRepeatedTasks) {
+		return doReSubmitCaseWithVariables(caseId, onlyStart, submitRepeatedTasks, null);
+	}
+
+	@Override
+	@RemoteMethod
+	public boolean doReSubmitCaseWithVariables(Integer caseId, boolean onlyStart, boolean submitRepeatedTasks, String variablesEncodedBase64) {
 		if (caseId == null) {
 			getLogger().warning("Case ID is not provided");
 			return false;
 		}
 
 		CaseProcInstBind bind = casesDAO.getCaseProcInstBindByCaseId(caseId);
-		return doReSubmit(bind, onlyStart, submitRepeatedTasks);
+		if (!doReSubmit(bind, onlyStart, submitRepeatedTasks)) {
+			return false;
+		}
+
+		if (StringUtil.isEmpty(variablesEncodedBase64)) {
+			return true;
+		}
+
+		return doSubmitVariables(caseId, variablesEncodedBase64);
+	}
+
+	@Override
+	@RemoteMethod
+	public boolean doSubmitVariables(Integer caseId, String variablesEncodedBase64) {
+		if (caseId == null) {
+			getLogger().warning("Case ID is not provided");
+			return false;
+		}
+		if (StringUtil.isEmpty(variablesEncodedBase64)) {
+			getLogger().warning("Variables not provided");
+			return false;
+		}
+
+		CaseProcInstBind bind = casesDAO.getCaseProcInstBindByCaseId(caseId);
+		if (bind == null) {
+			getLogger().warning("Process instance not found for case: " + caseId);
+			return false;
+		}
+
+		return doSubmitVariables(caseId, bind.getProcInstId(), variablesEncodedBase64);
 	}
 
 	private boolean doReSubmit(final CaseProcInstBind bind,
@@ -301,7 +354,7 @@ public class BPMManipulatorImpl extends DefaultSpringBean implements
 					BinaryVariable binVar = newTaskInstW.addAttachment(variable, fileName, description, identifier);
 					if (binVar == null) {
 						getLogger().warning(
-								"Failed to create new bin. variable (variable: "
+								"Failed to create new binary variable (variable: "
 										+ variable + ", file name: " + fileName
 										+ ", description: " + description
 										+ ", identifier: " + identifier
@@ -472,6 +525,201 @@ public class BPMManipulatorImpl extends DefaultSpringBean implements
 			getLogger().warning("Unable to find user by ID: " + id);
 		}
 		return null;
+	}
+
+	private boolean doSubmitVariables(Integer caseId, Long procInstId, String encoded) {
+		if (procInstId == null) {
+			return false;
+		}
+
+		Map<String, String> allVariables = new HashMap<>();
+		String decoded = new String(Base64Utils.decodeFromString(encoded));
+
+		ContentWithVariablesValues objLists = getObjectsLists(decoded);
+		if (objLists != null) {
+			decoded = objLists.content;
+			decoded = StringHandler.replace(decoded, ", , ", ", ");
+			Map<String, String> objListsData = objLists.variables;
+			MapUtil.append(allVariables, objListsData);
+		}
+
+		ContentWithVariablesValues variablesData = getVariables(decoded);
+		if (variablesData != null) {
+			decoded = variablesData.content;
+			Map<String, String> otherVariables = variablesData.variables;
+			MapUtil.append(allVariables, otherVariables);
+		}
+
+		ProcessInstanceW piW = bpmFactory.getProcessInstanceW(procInstId);
+		TaskInstanceW startTaskInstance = piW.getStartTaskInstance();
+
+		for (String name: allVariables.keySet()) {
+			String value = null;
+			Variable variable = null;
+			try {
+				value = allVariables.get(name);
+				VariableDataType dataType = VariableDataType.getVariableType(name);
+				variable = new Variable(name, dataType);
+				Object variableValue = null;
+				try {
+					variableValue = getConvertersFactory().createConverter(dataType).convert(value);
+				} catch (Exception e) {
+					getLogger().log(Level.WARNING, "Error converting '" + value + "' into real value for variable " + name, e);
+				}
+				if (variableValue == null) {
+					getLogger().warning("Failed to convert '" + value + "' into real value for variable " + name);
+					continue;
+				}
+
+				startTaskInstance.addVariable(variable, variableValue);
+			} catch (Exception e) {
+				getLogger().log(Level.WARNING, "Error adding variable '" + name + "' with value '" + value + "' (" + variable + ") for start task instance: " + startTaskInstance, e);
+				return false;
+			}
+		}
+
+		String description = allVariables.get(com.idega.block.process.business.ProcessConstants.CASE_DESCRIPTION);
+		if (caseId != null && !StringUtil.isEmpty(description)) {
+			try {
+			CaseBusiness caseBusiness = getServiceInstance(CaseBusiness.class);
+				Case theCase = caseBusiness.getCase(caseId);
+				theCase.setSubject(description);
+				theCase.store();
+			} catch (Exception e) {
+				getLogger().log(Level.WARNING, "Error setting description '" + description + "' for case: " + caseId, e);
+			}
+		}
+
+		return true;
+	}
+
+	private class ContentWithVariablesValues {
+
+		private String content;
+		private Map<String, String> variables;
+
+		private ContentWithVariablesValues(String content, Map<String, String> variables) {
+			this.content = content;
+			this.variables = variables;
+		}
+
+	}
+
+	private ContentWithVariablesValues getVariables(String content, String start, String end, List<String> afterEnd) {
+		if (StringUtil.isEmpty(content)) {
+			return null;
+		}
+
+		if (content.indexOf(start) == -1) {
+			return null;
+		}
+
+		if (content.startsWith(CoreConstants.CURLY_BRACKET_LEFT)) {
+			content = content.substring(1);
+		}
+		if (content.endsWith(CoreConstants.CURLY_BRACKET_RIGHT)) {
+			content = content.substring(0, content.length() - 1);
+		}
+
+		Map<String, String> data = new HashMap<>();
+
+		int startIndex = 0;
+		while (!StringUtil.isEmpty(content) && (startIndex = content.indexOf(start)) != -1) {
+			String name = CoreConstants.EMPTY;
+			int nameStartIndex = startIndex;
+			boolean foundName = false;
+			while (name != null && nameStartIndex > -1 && !foundName) {
+				if (name.startsWith(CoreConstants.SPACE)) {
+					name = name.trim();
+					foundName = true;
+					continue;
+				}
+				if (name.startsWith("{")) {
+					name = name.substring(1);
+					foundName = true;
+					continue;
+				}
+
+				nameStartIndex--;
+				name = content.substring(nameStartIndex < 0 ? 0 : nameStartIndex, startIndex);
+			}
+			if (StringUtil.isEmpty(name)) {
+				continue;
+			}
+
+			String tmpEnd = end;
+			int endIndex = content.indexOf(tmpEnd);
+			if (endIndex == 0) {
+				content = content.replaceFirst(end, CoreConstants.EMPTY);
+				startIndex = content.indexOf(start);
+				endIndex = content.indexOf(tmpEnd);
+			}
+			int dropEnd = 0;
+			if (ListUtil.isEmpty(afterEnd)) {
+				dropEnd = tmpEnd.length() - 1;
+			} else {
+				boolean foundEnd = false;
+				int shortestEnd = -1;
+				for (Iterator<String> afterEndIter = afterEnd.iterator(); (!foundEnd && afterEndIter.hasNext());) {
+					tmpEnd = end.concat(afterEndIter.next());
+					int tmpEndIndex = content.indexOf(tmpEnd);
+					if (tmpEndIndex == endIndex) {
+						foundEnd = true;
+					} else if (tmpEndIndex != -1) {
+						int valueEndIndex = startIndex + 1;
+						String value = CoreConstants.EMPTY;
+						boolean foundValue = false;
+						while (valueEndIndex < content.length() && !foundValue) {
+							valueEndIndex++;
+							value = content.substring(startIndex + 1, valueEndIndex);
+							foundValue = value.endsWith(tmpEnd);
+						}
+
+						if (valueEndIndex > -1 && foundValue) {
+							endIndex = valueEndIndex - tmpEnd.length();
+							shortestEnd = shortestEnd < 0 ?
+								endIndex :
+								shortestEnd > endIndex ? endIndex : shortestEnd;
+						}
+					}
+				}
+				endIndex = shortestEnd > -1 ? shortestEnd : endIndex;
+			}
+
+			endIndex = endIndex < 0 ? content.length() : endIndex;
+
+			if (startIndex < 0 || endIndex < 0 || endIndex < startIndex || (startIndex + 1 > endIndex + dropEnd)) {
+				getLogger().warning("Failed to find end index (end symbol: '" + end + "') for variable '" + name + "' in " + content);
+				return null;
+			}
+
+			String value = content.substring(startIndex + 1, endIndex + dropEnd);
+			if (StringUtil.isEmpty(value)) {
+				getLogger().warning("Failed to find value for variable '" + name + "' in " + content);
+				return null;
+			}
+
+			data.put(name, value);
+
+			content = StringHandler.replace(content, name.concat(CoreConstants.EQ).concat(value), CoreConstants.EMPTY);
+			if (content.startsWith(end)) {
+				content = content.replaceFirst(end, CoreConstants.EMPTY);
+			}
+		}
+		return new ContentWithVariablesValues(content, data);
+	}
+
+	private ContentWithVariablesValues getVariables(String content) {
+		List<String> afterEnd = new ArrayList<>();
+		for (VariableInstanceType type: VariableInstanceType.values()) {
+			afterEnd.add(type.getPrefix());
+		}
+
+		return getVariables(content, "=", ", ", afterEnd);
+	}
+
+	private ContentWithVariablesValues getObjectsLists(String content) {
+		return getVariables(content, "=[{", "]}}],", null);
 	}
 
 }
