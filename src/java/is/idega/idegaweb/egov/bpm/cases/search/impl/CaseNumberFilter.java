9c14c240559e93d0094467b7e3b0061a6b2e5fd3
Valdas Žemaitis <valdas@idega.com>
2018-03-14 10:38:03 +0200

    Using set; changed how case number is printed in logs

--- src/java/is/idega/idegaweb/egov/bpm/cases/search/impl/CaseNumberFilter.java
+++ src/java/is/idega/idegaweb/egov/bpm/cases/search/impl/CaseNumberFilter.java
@@ -1,11 +1,10 @@
 package is.idega.idegaweb.egov.bpm.cases.search.impl;
 
-import is.idega.idegaweb.egov.cases.data.GeneralCase;
-import is.idega.idegaweb.egov.cases.data.GeneralCaseHome;
-
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 
 import javax.ejb.FinderException;
@@ -23,6 +22,9 @@ import com.idega.util.CoreUtil;
 import com.idega.util.ListUtil;
 import com.idega.util.StringUtil;
 
+import is.idega.idegaweb.egov.cases.data.GeneralCase;
+import is.idega.idegaweb.egov.cases.data.GeneralCaseHome;
+
 @Service
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CaseNumberFilter extends DefaultCasesListSearchFilter {
@@ -31,8 +33,8 @@ public class CaseNumberFilter extends DefaultCasesListSearchFilter {
 	public List<Integer> getSearchResults(List<Integer> casesIds) {
 		String caseNumber = getCaseNumber();
 		String loweredCaseNumber = caseNumber.toLowerCase(CoreUtil.getIWContext().getCurrentLocale());
-		List<Integer> casesByNumberIds = new ArrayList<Integer>();
-		
+		Set<Integer> casesByNumberIds = new HashSet<Integer>();
+
 		//	"BPM" cases
 		List<Long> bpmCases = null;
 		try {
@@ -41,39 +43,36 @@ public class CaseNumberFilter extends DefaultCasesListSearchFilter {
 			getLogger().log(Level.WARNING, "Exception while resolving case ids by case number = " + loweredCaseNumber, e);
 		}
 		if (ListUtil.isEmpty(bpmCases)) {
-			getLogger().log(Level.INFO, "No BPM cases found by number: " + caseNumber);
+			getLogger().info("No BPM cases found by number: " + caseNumber);
 		} else {
-			getLogger().log(Level.INFO, "BPM cases by number (" + caseNumber + "): " + bpmCases);
+			getLogger().info("BPM cases by number '" + caseNumber + "': " + bpmCases);
 			casesByNumberIds.addAll(getConvertedFromNumbers(bpmCases));
 		}
-		
+
 		//	Old cases
-		List<Integer> simpleCases = CasesRetrievalManager.CASE_LIST_TYPE_USER.equals(getCaseListType()) ? getUserCasesByNumber(loweredCaseNumber) :
-			getGeneralCasesByNumber(loweredCaseNumber);
+		List<Integer> simpleCases = CasesRetrievalManager.CASE_LIST_TYPE_USER.equals(getCaseListType()) ?
+				getUserCasesByNumber(loweredCaseNumber) :
+				getGeneralCasesByNumber(loweredCaseNumber);
 		if (ListUtil.isEmpty(simpleCases)) {
-			getLogger().log(Level.INFO, "No simple cases found by number: " + caseNumber);
+			getLogger().info("No simple cases found by number: " + caseNumber);
 		} else {
-			getLogger().log(Level.INFO, "Simple cases by number (" + caseNumber + "): " + simpleCases);
-			for (Integer id: simpleCases) {
-				if (!casesByNumberIds.contains(id)) {
-					casesByNumberIds.add(id);
-				}
-			}
+			getLogger().info("Simple cases by number '" + caseNumber + "': " + simpleCases);
+			casesByNumberIds.addAll(simpleCases);
 		}
-		
+
 		if (ListUtil.isEmpty(casesByNumberIds)) {
-			getLogger().log(Level.INFO, "No cases found by number: " + caseNumber);
+			getLogger().info("No cases found by number: " + caseNumber);
 		} else {
-			getLogger().log(Level.INFO, "Cases found by number (" + caseNumber + "): " + casesByNumberIds);
+			getLogger().info("Cases found by number '" + caseNumber + "': " + casesByNumberIds);
 		}
-		return casesByNumberIds;
+		return new ArrayList<>(casesByNumberIds);
 	}
 
 	@Override
 	protected String getInfo() {
 		return "Looking for cases by number: " + getCaseNumber();
 	}
-	
+
 	private List<Integer> getGeneralCasesByNumber(String caseNumber) {
 		GeneralCaseHome caseHome = null;
 		try {
@@ -84,7 +83,7 @@ public class CaseNumberFilter extends DefaultCasesListSearchFilter {
 		if (caseHome == null) {
 			return null;	//	Unable to search for general cases
 		}
-		
+
 		Collection<Integer> casesByNumber = null;
 		try {
 			casesByNumber = caseHome.getCasesIDsByCriteria(caseNumber, null, null, null, null, null, null, null, true);
@@ -94,26 +93,26 @@ public class CaseNumberFilter extends DefaultCasesListSearchFilter {
 		if (ListUtil.isEmpty(casesByNumber)) {
 			return null;	//	No results
 		}
-		
+
 		return getUniqueIds(casesByNumber);
 	}
-	
+
 	private List<Integer> getUserCasesByNumber(String number) {
 		CaseHome caseHome = getCaseHome();
 		if (caseHome == null) {
 			return null;
 		}
-		
+
 		Collection<Integer> casesByNumber = null;
 		try {
 			casesByNumber = caseHome.findIDsByCriteria(number, null, null, null, null, null, null, null, true);
 		} catch (FinderException e) {
 			e.printStackTrace();
 		}
-		
+
 		return getUniqueIds(casesByNumber);
 	}
-	
+
 	private CaseHome getCaseHome() {
 		try {
 			return (CaseHome) IDOLookup.getHome(Case.class);
